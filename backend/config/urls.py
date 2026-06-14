bddb4bc5d2ea5b694ebb6884dfabc879fc29bbc9
Rafał Peisert <rafalmp@users.noreply.github.com>
2026-06-14 19:54:12 +0200

    feat: serve media files by dev server

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,9 +2,13 @@
 
 from __future__ import annotations
 
+from django.conf import settings
 from django.contrib import admin
 from django.http import HttpRequest, JsonResponse
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
+
+
 
 
 def healthz(_request: HttpRequest) -> JsonResponse:
@@ -17,3 +21,14 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("api.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(
+            r"^media/(?P<path>.*)$",
+            serve,
+            {
+                "document_root": settings.MEDIA_ROOT,
+            },
+        ),
+    ]
